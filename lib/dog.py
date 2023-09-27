15eb3de082c0d62720312b0a92e84db903d7487d
kipmaiyo <danielkipmaiyo44@gmail.com>
2023-09-27 13:54:20 +0300

    Assignment Done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+   def __init__(self, name, breed = "Mutt"):
+      self.name = name
+      self.breed = breed
+   
+   pass
+
+# class Dog:
+#     def __init__(self, name):
+#         print(f"{name} is born")
+
+#     def bark(self):
+#         print("Woof!")  
+
+#     def showing_self(self):
+#         return self   
+
+# fido = Dog("Fido")
+# lassie = Dog("Lazio")
+
+# fido is fido.showing_self()
